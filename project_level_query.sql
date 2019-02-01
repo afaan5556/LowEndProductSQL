84af2e0ea246775fce9a20f8cae19752c41d429d
anaqvi <anaqvi@anaqvis-MacBook-Pro.local>
2019-02-01 11:59:38 -0800

    Add casting to main line and project level queries

--- project_level_query.sql
+++ project_level_query.sql
@@ -1,17 +1,20 @@
 -- This makes a wide table of workday and airtable data
 -- When we join this to dpr if you mult the floor_usf by the per_usf #'s you should get to the spend for that line item, in that cpi, on that floor
 -- You should use the ifnull(floor_usf, 0.00001) for you usf multiplier in case there is an active project that has no usf then all the cost will be allocated evenly across the floors (i think) 
--- spot check that when you total everything up it matches the airtable and workday totals 
+-- spot check that when you total everything up it matches the airtable and workday totals
+
 -- Find the latest revision of the airtable budget
 WITH at_revision
 AS (SELECT
   id,
   ROW_NUMBER() OVER (PARTITION BY project_code ORDER BY submitted_date DESC) AS rn
 FROM playground.airtable_budgets),
+
 -- Find Project total USF
 project_total
 AS (SELECT
   SUM(COALESCE(floor_usf, 0.00001)) AS project_usf, -- this is basically ifnull()
+  SUM(COALESCE(floor_desks, 1)) AS project_desks, -- this is basically ifnull()
   project_uuid
 FROM stargate_bi_stargate.mv_desk_projections_v2
 GROUP BY project_uuid
@@ -23,10 +26,11 @@ airtable
 AS (SELECT
   p.uuid AS project_uuid,
   pt.project_usf AS project_total_usf,
+  pt.project_desks AS project_total_desks,
   signed_off_date AS at_sign_date,
   COALESCE(REPLACE(REPLACE(UPPER(REPLACE(b.category, '_', ' ')), 'COSTS', 'COST'), ' ', ' '), 'OTHER') AS at_cpi, -- ADDED CPI TRANSLATOR STUFF HERE 
   LOWER(b.description) AS at_line_item,
-  initial_cost / pt.project_usf AS at_budget_line_per_usf, -- get per usf # so when we join to dpr we mult by square feet on that floor/ area 
+  CAST(initial_cost AS DOUBLE PRECISION) / pt.project_usf AS at_budget_line_per_usf, -- get per usf # so when we join to dpr we mult by square feet on that floor/ area 
   currency AS at_currency
 FROM playground.airtable_budgets b
 LEFT JOIN at_revision
@@ -44,14 +48,15 @@ workday
 AS (SELECT
   p.uuid AS project_uuid,
   pt.project_usf AS project_total_usf,
+  pt.project_desks AS project_total_desks,  
   LOWER(purchase_item) AS jcr_line_item,
   cpi AS jcr_cpi,
   -- Actuals and projected jcr columns added
-  total_paid_amount_paid_expense_report_credit_card_intercompany_ / pt.project_usf AS jcr_actuals_per_usf,
-  total_projected_spend_po_supplier_contract_ / pt.project_usf AS jcr_projected_per_usf,
-  invoiced_amount / pt.project_usf AS jcr_invoiced_amount_per_usf,
-  expense_report / pt.project_usf AS jcr_expense_report_per_usf,
-  current_budget / pt.project_usf AS jcr_budget_line_per_usf
+  CAST(total_paid_amount_paid_expense_report_credit_card_intercompany_ AS DOUBLE PRECISION) / pt.project_usf AS jcr_actuals_per_usf,
+  CAST(total_projected_spend_po_supplier_contract_ AS DOUBLE PRECISION) / pt.project_usf AS jcr_projected_per_usf,
+  CAST(invoiced_amount AS DOUBLE PRECISION) / pt.project_usf AS jcr_invoiced_amount_per_usf,
+  CAST(expense_report AS DOUBLE PRECISION) / pt.project_usf AS jcr_expense_report_per_usf,
+  CAST(current_budget AS DOUBLE PRECISION) / pt.project_usf AS jcr_budget_line_per_usf
 FROM workday_development.mv_workday_job_cost_report jcr
 LEFT JOIN stargate_bi_stargate.bi_project p
   ON p.id = jcr.project_id
@@ -73,11 +78,11 @@ sg_revision_detail
 AS (SELECT
   rd.revision_uuid,
   MAX(rd.currency) AS currency,
-  SUM(rd.final_cost) AS budget,
+  SUM(CAST(rd.final_cost AS DOUBLE PRECISION)) AS budget,
   COALESCE(REPLACE(REPLACE(UPPER(REPLACE(rd.category, '_', ' ')), 'COSTS', 'COST'), '  ', ' '), 'OTHER') AS cpi_categories,
   LOWER(rd.cost_description) AS cost_description,
   CASE
-    WHEN SUM(rd.final_cost) != SUM(rd.initial_cost) THEN SUM(rd.initial_cost ::NUMERIC(30, 20)) / SUM(rd.final_cost)
+    WHEN SUM(CAST(rd.final_cost AS DOUBLE PRECISION)) != SUM(CAST(rd.initial_cost AS DOUBLE PRECISION)) THEN CAST(SUM(CAST(rd.initial_cost AS DOUBLE PRECISION)) / SUM(CAST(rd.final_cost AS DOUBLE PRECISION)) AS DOUBLE PRECISION)
   END AS rate
 FROM stargate_modules_budget_production.revision_detail rd
 WHERE rd._fivetran_deleted = FALSE AND NOT cpi_categories ~ '^[0-9\.]+$' AND NOT cpi_categories ~ '#sV/0!'
@@ -92,6 +97,7 @@ stargate
 AS (SELECT
   b.project_uuid,
   pt.project_usf AS project_total_usf,
+  pt.project_desks AS project_total_desks,
   sg_revision_detail.cpi_categories AS sg_cpi,
   sg_revision_detail.cost_description AS sg_line_item,
   (CASE
@@ -101,6 +107,7 @@ AS (SELECT
     ELSE sg_revision_detail.rate
   END) AS sg_exchange_rate,
   date_trunc('day', sg_revision.created_date) AS sg_budget_date,
+  
   /*Apply rate to budget*/
   sg_revision_detail.budget * (CASE
     WHEN sg_revision_detail.rate IS NULL AND
@@ -108,7 +115,9 @@ AS (SELECT
     WHEN sg_revision_detail.rate IS NULL THEN r.rate
     ELSE sg_revision_detail.rate
   END) / pt.project_usf AS sg_cpi_budget_per_usf,
+  
   sg_revision_detail.currency AS sg_currency
+  
 FROM stargate_modules_budget_production.budget b
 LEFT JOIN sg_revision
   ON b.uuid = sg_revision.budget_uuid
@@ -132,6 +141,7 @@ AND pt.project_uuid IS NOT NULL -- only if there is a project with more than 0 s
 detail AS (SELECT
   COALESCE(airtable.project_uuid, COALESCE(workday.project_uuid, stargate.project_uuid)) AS project_uuid, -- We won't get 100% matches so this makes sure this columns is populated all the way down
   COALESCE(airtable.project_total_usf, COALESCE(workday.project_total_usf, stargate.project_total_usf)) AS project_total_usf,
+  COALESCE(airtable.project_total_desks, COALESCE(workday.project_total_desks, stargate.project_total_desks)) AS project_total_desks,
   LOWER(COALESCE(airtable.at_cpi, COALESCE(workday.jcr_cpi, stargate.sg_cpi))) AS cpi, -- same here 
   COALESCE(airtable.at_line_item, COALESCE(workday.jcr_line_item, stargate.sg_line_item)) AS line_item, -- and here
   airtable.at_sign_date,
@@ -159,9 +169,10 @@ GROUP BY 1,
          3,
          4,
          5,
-         7,
+         6,
          8,
-         10
+         9,
+         11
 ORDER BY 1, 2, 3
 )
 
@@ -171,6 +182,9 @@ SELECT
   MAX(at_sign_date) AS at_sign_date,
   SUM(at_budget_line_per_usf) AS at_budget_line_per_usf_total,
   MAX(at_currency) AS at_currency,
+  
+  MAX(project_total_usf) AS project_total_usf,
+  MAX(project_total_desks) AS project_total_desks,
 
   MAX(sg_budget_date) AS sg_budget_date,
   SUM(sg_cpi_budget_per_usf) AS sg_cpi_budget_per_usf_total,
@@ -182,4 +196,5 @@ SELECT
   SUM(jcr_actuals_per_usf) AS jcr_actuals_per_usf_total,
   SUM(jcr_projected_per_usf) AS jcr_projected_per_usf_total
 FROM detail
+
 GROUP BY project_uuid
